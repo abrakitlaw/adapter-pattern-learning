755bb0155d0fa3d483b76f1f8dc83d01e7ca3f81
abrakitlaw <gkitlaw@gmail.com>
2019-10-01 17:03:05 +0700

    Implement object and class adapter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# adapter-pattern-learning
+# Adapter Pattern
+The Adapter Pattern is one of the structural design pattern that converts the interface of a class into another interface of clients expect. Adapter lets classes work together that
+couldn't otherwise because of incompatible interfaces.
+
+There are two kinds of adapters: object adapters and class adapters.
+
+1. Class Adapter - This form uses Java inheritance and extends the source interface , in our case Socket class.
+2. Object Adapter - This form uses Java composition and adapter contains the source object.
+
+
+The only difference about these two adapters is that with class adapter we subclass the Target and the Adaptee, while with object adapter
+we use composition to pass request to an Adaptee.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        testObjectAdapter();
+        testClassAdapter();
+    }
+
+    private static void testObjectAdapter() {
+        SocketAdapter socketObjectAdapter = new SocketObjectAdapterObjectImpl();
+        Volt volt3 = getVolt(socketObjectAdapter, 3);
+        Volt volt12 = getVolt(socketObjectAdapter, 12);
+        Volt volt120 = getVolt(socketObjectAdapter, 120);
+        System.out.println(volt3.getVolts() + " Volts using object adapter");
+        System.out.println(volt12.getVolts() + " Volts using object adapter");
+        System.out.println((volt120.getVolts() + " Volts using object adapter"));
+    }
+
+    private static void testClassAdapter() {
+        SocketAdapter socketClassAdapter = new SocketClassAdapterImpl();
+        Volt volt3 = getVolt(socketClassAdapter, 3);
+        Volt volt12 = getVolt(socketClassAdapter, 12);
+        Volt volt120 = getVolt(socketClassAdapter, 120);
+        System.out.println();
+        System.out.println();
+        System.out.println(volt3.getVolts() + " Volts using class adapter");
+        System.out.println(volt12.getVolts() + " Volts using class adapter");
+        System.out.println((volt120.getVolts() + " Volts using class adapter"));
+    }
+
+    private static Volt getVolt(SocketAdapter socketAdapter, int i) {
+        switch (i) {
+            case 3:
+                return socketAdapter.get3Volt();
+            case 12:
+                return socketAdapter.get12Volt();
+            default:
+                return socketAdapter.get120Volt();
+        }
     }
 }
